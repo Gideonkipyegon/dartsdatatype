4fb7b505a28e0d77d8e705dae2373a74b78a302d
Gideonkipyegon <gideonkipyegon837@gmail.com>
2024-03-07 12:43:07 +0300

    first commit

---
+++ datatypes.dart
@@ -0,0 +1,27 @@
+void main() {
+  // Integers: Represents whole numbers
+  int age = 25;
+
+  // Double: Represents floating-point numbers
+  double height = 5.9;
+
+  // String: Represents a sequence of characters
+  String name = "John Doe";
+
+  // List: Represents an ordered collection of objects
+  List<int> numbers = [1, 2, 3, 4, 5];
+
+  // Map: Represents a collection of key-value pairs
+  Map<String, dynamic> person = {
+    'name': 'Alice',
+    'age': 30,
+    'isStudent': true,
+  };
+
+  // Printing out the values
+  print('Age: $age'); // Age: 25
+  print('Height: $height'); // Height: 5.9
+  print('Name: $name'); // Name: John Doe
+  print('Numbers: $numbers'); // Numbers: [1, 2, 3, 4, 5]
+  print('Person: $person'); // Person: {name: Alice, age: 30, isStudent: true}
+}
